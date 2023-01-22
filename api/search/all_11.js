bd3b14c4374744ab223434c01b354d4cf31d7edd
herculanodavi <herculanodavi@users.noreply.github.com>
2023-01-22 12:28:16 +0000

    deploy: 185ced69f0001589e288f6e22e143c32faa46e8a

--- api/search/all_11.js
+++ api/search/all_11.js
@@ -8,6 +8,6 @@ var searchData=
   ['uptime_5fcounter_5',['uptime_counter',['../classembed_1_1experimental_1_1uptime__counter.html',1,'embed::experimental::uptime_counter'],['../classembed_1_1experimental_1_1uptime__counter.html#a3c08da81ae8b94163a0986ecc626600f',1,'embed::experimental::uptime_counter::uptime_counter()']]],
   ['uptime_5fcounter_2ehpp_6',['uptime_counter.hpp',['../uptime__counter_8hpp.html',1,'']]],
   ['uptime_5ft_7',['uptime_t',['../structembed_1_1counter_1_1uptime__t.html',1,'embed::counter']]],
-  ['util_2ehpp_8',['util.hpp',['../serial_2util_8hpp.html',1,'(Global Namespace)'],['../spi_2util_8hpp.html',1,'(Global Namespace)'],['../i2c_2util_8hpp.html',1,'(Global Namespace)'],['../counter_2util_8hpp.html',1,'(Global Namespace)']]],
+  ['util_2ehpp_8',['util.hpp',['../spi_2util_8hpp.html',1,'(Global Namespace)'],['../serial_2util_8hpp.html',1,'(Global Namespace)'],['../i2c_2util_8hpp.html',1,'(Global Namespace)'],['../counter_2util_8hpp.html',1,'(Global Namespace)']]],
   ['utility_9',['Utility',['../group__utility.html',1,'']]]
 ];
